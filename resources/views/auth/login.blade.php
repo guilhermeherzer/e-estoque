5f5951c847838c8f6bbdabecfc17d86ef04b5976
Guilherme Herzer <guilherme.mr@live.com>
2020-03-19 18:30:01 -0300

    logo adicionada ao form de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,16 +17,18 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-xl-5 col-lg-6 col-md-4">
+            <div class="col-xl-10 col-lg-12 col-md-8">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
-                            <div class="col-lg-12">
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <img src="{{ asset('assets/img/logo.png') }}" style="margin: 0; position: absolute; left: 23%; top: 15%;">
+                            </div>
+                            <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">e-Com</h1>
                                     </div>
                                     <form  method="POST" action="{{ route('login') }}" class="user">
                                         @csrf
