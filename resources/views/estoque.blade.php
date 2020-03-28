86ea9d601b09a93df6cd302868327e8237fcc490
Guilherme Herzer <guilherme.mr@live.com>
2020-03-28 00:09:54 -0300

    titulo da pagina alterado

--- resources/views/estoque.blade.php
+++ resources/views/estoque.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">Entrada de Produtos</h1>
+		<h1 class="h3 mb-0 text-gray-800">Estoque</h1>
 		<a href="#" data-toggle="modal" data-target="#entradaModal" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Cadastrar Entrada</a>
 	</div>
 	<!-- DataTales Example -->
