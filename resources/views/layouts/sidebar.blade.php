b935a083a8a0784ccd564df54156ad3459096411
Guilherme Herzer <guilherme.mr@live.com>
2020-03-30 16:47:06 -0300

    sidebar via json

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,3 +1,7 @@
+		<?php
+			$itens = file_get_contents('C:/Users/guilh/e-com/resources/views/layouts/sidebar.json');
+			$menus = json_decode($itens);
+		?>
 		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 			<!-- Sidebar - Brand -->
 			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
@@ -17,69 +21,22 @@
 			</li>
 			<!-- Divider -->
 			<hr class="sidebar-divider">
-			<!-- Heading -->
-			<div class="sidebar-heading">
-				Produtos
-			</div>
-			<!-- Nav Item - Charts -->
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('produtos') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Produtos</span>
-				</a>
-				<a class="nav-link" href="{{ route('produtos') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Categorias</span>
-				</a>
-				<a class="nav-link" href="{{ route('produtos') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Marcas</span>
-				</a>
-			</li>
-			<!-- Divider -->
-			<hr class="sidebar-divider">
-			<!-- Heading -->
-			<div class="sidebar-heading">
-				Fornecedores
-			</div>
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('fornecedores') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Fornecedores</span>
-				</a>
-			</li>
-			<!-- Divider -->
-			<hr class="sidebar-divider">
-			<!-- Heading -->
-			<div class="sidebar-heading">
-				Entradas e Saidas
-			</div>
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('entradas') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Entradas</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('saidas') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Saidas</span>
-				</a>
-			</li>
-			<!-- Divider -->
-			<hr class="sidebar-divider">
-			<!-- Heading -->
-			<div class="sidebar-heading">
-				Estoque
-			</div>
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('estoque') }}">
-					<i class="fas fa-chevron-circle-right"></i>
-					<span>Estoque</span>
-				</a>
-			</li>
-			<!-- Divider -->
-			<hr class="sidebar-divider d-none d-md-block">
+			@foreach($menus->menus as $m)
+				<!-- Heading -->
+				<div class="sidebar-heading">
+					{{ $m->span }}
+				</div>
+					@foreach($m->sub as $s)
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route($s->href) }}">
+								<i class="fas fa-chevron-circle-right"></i>
+								<span>{{ $s->span }}</span>
+							</a>
+						</li>
+					@endforeach
+				<!-- Divider -->
+				<hr class="sidebar-divider">
+			@endforeach
 			<!-- Sidebar Toggler (Sidebar) -->
 			<div class="text-center d-none d-md-inline">
 				<button class="rounded-circle border-0" id="sidebarToggle"></button>
